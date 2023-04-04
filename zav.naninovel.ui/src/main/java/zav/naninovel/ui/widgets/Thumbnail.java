0f98cca33e296669a1c2709930eb25a697368e40
Zavarov <25961215+Zavarov@users.noreply.github.com>
2023-04-04 22:50:26 +0200

    Delete GridComposite class. Layouts should be set explicitly.

--- zav.naninovel.ui/src/main/java/zav/naninovel/ui/widgets/Thumbnail.java
+++ zav.naninovel.ui/src/main/java/zav/naninovel/ui/widgets/Thumbnail.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
@@ -15,7 +16,9 @@ import zav.naninovel.core.model.SaveGame;
 
 public class Thumbnail {
 	public Thumbnail(Composite parent, SaveGame model) {
-		Composite composite = new GridComposite(parent, SWT.NONE);
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new GridLayout());
+		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
 		Label thumbnail = new Label(composite, SWT.NONE);
 		thumbnail.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
